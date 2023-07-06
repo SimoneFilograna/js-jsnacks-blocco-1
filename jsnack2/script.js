a5670d77ec6fb2b2af288ea61ac77368121bcea8
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-06 17:32:03 +0200

    fixed

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,13 +1,17 @@
-const number = [3, 5, 2, 6, 8, 10, 23, 21, 9, 30, 70, 65]
+const number = [3, 5, 2, 6, 8, 10, 23, 21, 9, 30, 70, 65];
 console.log(number);
 
+let sum = 0;
 
-for( let i = 0; i < number.length; i++){
-    i = (i + 1) ;
-    if ( i >= number.length){
-        break
-    }
-    // console.log(i)
-    let oddNumber = number[i]
-    console.log(oddNumber)   
+for( let i = 1; i < number.length; i+=2){
+    console.log("*****");
+    // // console.log(i)
+    let oddNumber = number[i];
+    console.log(oddNumber);
+
+    sum += oddNumber;                 
+    console.log(i);
 }
+
+console.log("**********");
+console.log(sum);

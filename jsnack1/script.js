1df273acf7e4628d0796f23081e0991951a3a70c
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-07 10:17:37 +0200

    release

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,9 +3,12 @@ const surnameList = ["Leone", "Giuri", "Filograna", "Corluccio", "Presta", "Corr
 console.log(nameList);
 console.log(surnameList);
 
+const listaInvitati = [];
 
-for(let i = 0; i < nameList.length; i++){
+
+for(let i = 0; i < 30; i++){
     const randomNameIndex = Math.floor(Math.random() * 9) ;
+    // il +9 indica la lunghezza del coprrispottivo array. In questo caso name.length
     const randomSurnameIndex = Math.floor(Math.random() * 9) ;
     
     const currentName = nameList[randomNameIndex];
@@ -13,6 +16,9 @@ for(let i = 0; i < nameList.length; i++){
 
     const completeName = (currentName + " " + currentSurname);
     console.log(completeName)
+
+    listaInvitati.push(completeName);
 }
 
+console.log(listaInvitati)
 
